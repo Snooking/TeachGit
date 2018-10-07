767ce7d2ed877f6681a676b016393d356b9795de
Snooking <adriankon123@wp.pl>
2018-10-07 22:35:27 +0200

    Done

--- TeachGit/TeachGit/Program.cs
+++ TeachGit/TeachGit/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Now we're on Testing4"); 
+            System.Console.WriteLine("Now we're on Testing4");
+            System.Console.WriteLine("Did something else"); 
         }
     }
 }
